ad80f6b26920ef97c2151dd6d2435cc76fc00b48
allImran <badhan19@gmail.com>
2023-02-12 20:40:25 +0600

    page markup

--- pages/salary-certificate.vue
+++ pages/salary-certificate.vue
@@ -1,5 +1,63 @@
 <template>
     <div>
         <Tab></Tab>
+        <p class="text-center font-bold text-md mt-10">Certificate of deduction of tax under the head “Salaries” under section 58 of the Income Tax ordinance 1984 and rule 18(1) of the Income Tax rule 1984.</p>
+        <p class="text-sm mt-10">(i) The name and address of the person or the authority paying such amount:</p>
+        <div class="mt-5 font-bold text-sm">
+            <p class="text-center">Company Name</p>
+            <p class="text-center">Company Address</p>
+            <p class="text-center">Location</p>
+        </div>
+
+        <div class="flex gap-x-10 text-sm mt-10">
+            <p>(ii) The period for which the payment is made:</p>
+            <div class="flex">
+                <p>Income Year:</p>
+                <strong>2023</strong>
+            </div>
+        </div>
+
+        <div class="text-sm font-semibold">
+            (iii) Name, Designation & TIN of the employee:
+            <table class="ml-10">
+                <tr>
+                    <td>Name:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Id:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Designation:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Date of Joining:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>TIN:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Gender:</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+
+        <p class="text-sm">(iv) The breakdown of payment</p>
+        <p class="text-sm">(x) Name, Designation & Signature, along with date of the person issuing the certificate.</p>
+        <div class="flex justify-between text-sm mt-14">
+            <div>
+                <p>Authorized Person Name:</p>
+                <p>Designation:</p>
+            </div>
+            <div class="border-t border-gray-700 w-60">
+                <p>Signature</p>
+                <p>Date</p>
+            </div>
+        </div>
     </div>
 </template>

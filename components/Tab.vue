4e9f71c5289685e264f41fe22dcb69aa9293cdac
allImran <badhan19@gmail.com>
2023-02-12 19:04:07 +0600

    salary certificate page add

---
+++ components/Tab.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-96">
+        <ul class="flex border-b border-gray-200 text-center">
+            <li class="flex-1">
+                <a
+                    class="relative block border-t border-l border-r border-gray-200 bg-white p-4 text-sm font-medium"
+                    href=""
+                >
+                <span class="absolute inset-x-0 -bottom-px h-px w-full bg-white"></span>
+                    Salary certificate
+                </a>
+            </li>
+
+            <li class="flex-1 pl-px">
+                <a
+                    class="block bg-red-50 p-4 text-sm font-medium text-gray-500 ring-1 ring-inset ring-white"
+                    href=""
+                >
+                    Tax card
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>

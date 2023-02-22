92770fa3910de3eaed32978fd0dd189077bae5e4
allImran <badhan19@gmail.com>
2023-02-23 00:59:23 +0600

    add head dynamically

--- components/PaymentTable.vue
+++ components/PaymentTable.vue
@@ -23,12 +23,39 @@
                         <TableTd :text="item.taxable_salary"/>
                     </template>
                 </TableTr>
+                
+                <TableTr v-for="head in heads" :key="head.title">
+                    <td class="py-3 px-6 text-left">
+                        <select v-model="head.value" class="bg-transparent">
+                            <option :value="null">{{ head.title }}</option>
+                            <option
+                                v-for="item in headItem"
+                                :key="item.key"
+                                :value="item.key"
+                            >
+                                {{ item.label }}
+                            </option>
+                        </select>
+                    </td>
+                    <template v-if="head.value">
+                        <td class="text-right">
+                            <input 
+                                type="number"
+                                class="bg-transparent py-3 px-6 text-right"
+                                v-model="getItem(head.value).gross_income"
+                                @input="$emit('onInput')"
+                            >
+                        </td>
+                        <TableTd :text="getItem(head.value).exemption"/>
+                        <TableTd :text="getItem(head.value).taxable_salary"/>
+                    </template>
+                </TableTr>
+
                 <TableTr v-if="totalIncome" customBg="bg-gray-100 font-bold">
                     <TableTd :isNumber="false" :text="totalIncome.label"/>
                     <TableTd :text="totalIncome.gross_income"/>
                     <TableTd :text="totalIncome.exemption"/>
                     <TableTd :text="totalIncome.taxable_salary"/>
-                    <!-- {{ totalIncome }} -->
                 </TableTr>
             </tbody>
         </TableWrapper>
@@ -38,6 +65,7 @@
 <script setup>
 import { first } from 'lodash'
 import {computed} from 'vue'
+import headItem from '~/data/head.js'
 
 const props = defineProps({
     salaryForm: Array,
@@ -45,7 +73,23 @@ const props = defineProps({
 })
 
 const totalIncome = computed(() => first(props.salaryReturn))
+const heads = ref([
+    {
+        title: 'Head 1',
+        value: null
+    },
+    {
+        title: 'Head 2',
+        value: null
+    },
+    {
+        title: 'Head 3',
+        value: null
+    }
+])
 const thArr = [' Heads of income', 'Gross Income', 'Tax Exempted', 'Taxable Income']
 const validKeys = ['basic_pay', 'house_rent', 'medical_allowance', 'conveyance_allowance', 'festival_bonus', 'other_allowance']
 const showItem = (key) => validKeys.includes(key)
+
+const getItem = (_key) => props.salaryForm.find(({key}) => key === _key)
 </script>
---
+++ data/head.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        key: 'special_bonus',
+        label: 'Special bonus/ pay'
+    },
+    {
+        key: 'arrear_pay',
+        label: 'Arrear pay (if not taxed earlier)'
+    },
+    {
+        key: 'festival_bonus',
+        label: 'Festival bonus'
+    },
+    {
+        key: 'dearness_allowance',
+        label: 'Dearness allowance'
+    },
+]

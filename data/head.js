76aeea1bc2f0a1b594d6a01549917fdb1bcd0b03
allImran <badhan19@gmail.com>
2023-02-24 10:28:13 +0600

    add heads

--- data/head.js
+++ data/head.js
@@ -15,4 +15,32 @@ export default [
         key: 'dearness_allowance',
         label: 'Dearness allowance'
     },
+    {
+        key: 'house_maintenance',
+        label: 'House maintenance'
+    },
+    {
+        key: 'leave_allowance',
+        label: 'Leave Allowance/ Leave Fair Assistance (LFA)'
+    },
+    {
+        key: 'dearness_allowance',
+        label: 'Dearness allowance'
+    },
+    {
+        key: 'overtime_allowance',
+        label: 'Overtime allowance'
+    },
+    {
+        key: 'bonus',
+        label: 'Bonus / Ex-gratia'
+    },
+    {
+        key: 'other_allowance',
+        label: 'Other allowances'
+    },
+    {
+        key: 'others',
+        label: 'Other, if any (give detail)s'
+    }
 ]

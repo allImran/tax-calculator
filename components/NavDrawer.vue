c1a3af39f8475ed0ec25b16b112230c5aa6ce8a7
allImran <badhan19@gmail.com>
2023-02-24 09:43:12 +0600

    slide effect add

--- components/NavDrawer.vue
+++ components/NavDrawer.vue
@@ -1,17 +1,19 @@
 <template>
-<div
-    v-if="show"
-      style="height: calc(100% - 70px)" 
-      class="sm:hidden absolute top-[70px] left-0 w-70 bg-red-700 opacity-90 px-3 py-4"
-    >
-    <div @click="toggleSidebar">
-        <CloseIcon class="cursor-pointer text-white mb-2"/>
+  <Transition name="slide-fade">
+    <div
+      v-if="show"
+        style="height: calc(100% - 70px)" 
+        class="sm:hidden absolute top-[70px] left-0 w-70 bg-red-700 opacity-90 px-3 py-4"
+      >
+      <div @click="toggleSidebar">
+          <CloseIcon class="cursor-pointer text-white mb-2"/>
+      </div>
+      <div class="uppercase text-sm font-bold text-white flex flex-col space-y-2">
+        <a href="https://app.taxhouse.com.bd/register">Create taxhouse account</a>
+        <a href="https://app.taxhouse.com.bd/login">Log in</a>
+      </div>
     </div>
-    <div class="uppercase text-sm font-bold text-white flex flex-col space-y-2">
-      <a href="https://app.taxhouse.com.bd/register">Create taxhouse account</a>
-      <a href="https://app.taxhouse.com.bd/login">Log in</a>
-    </div>
-    </div>
+  </Transition>
 </template>
 
 <script setup>
